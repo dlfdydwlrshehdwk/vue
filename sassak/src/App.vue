3ae752e9441b1d9a63f0a168946a20340a4f46d2
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-08-27 18:18:47 +0900

    1

---
+++ sassak/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>{{ add(0) }}</div>
+    <div>{{ add(1) }}</div>
+    <div>{{ addadd(50) }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      age : 30
+    }
+  },
+  methods:{
+    add(num=0){
+      console.log(num);
+      // this를 써야 vue가 가진 데이터에 접근가능!
+      return this.age + num;
+    },
+    addadd(num){
+      return this.add(num)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
