df28a4353db5acc8119737a64c0d3587ac018e7e
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-08-27 19:46:28 +0900

    1

--- sassak/src/App.vue
+++ sassak/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <div>{{ add(0) }}</div>
-    <div>{{ add(1) }}</div>
-    <div>{{ addadd(50) }}</div>
+      <div>
+        <h1>Watchers</h1>
+        <div>{{ money }}</div>
+        <button @click="money = money + 100">+100</button>
+        <button @click="money = money - 100">-100</button>
+      </div>
+      <h3>{{ receit }}</h3>
+      <button @click="receit.food+=500">receit 조작</button>
+      <hr>
+      <input type="text" v-model="name">
   </div>
 </template>
 
@@ -11,21 +18,46 @@ export default {
   name: 'App',
   data(){
     return{
-      age : 30
+      name : "kim",
+      money : 0,
+      receit : {
+        food : 3000,
+        fee : 200,
+        fare : 10000
+      }
     }
   },
-  methods:{
-    add(num=0){
-      console.log(num);
-      // this를 써야 vue가 가진 데이터에 접근가능!
-      return this.age + num;
+  // data에서 값이 바뀔때마다 체크를해줌(바뀐값, 바뀌기전값)
+  watch:{
+    money(newValue, oldValue){
+      console.log(newValue,oldValue)
     },
-    addadd(num){
-      return this.add(num)
+    // 객체데이터면 이렇게 해야한다고함
+    receit : {
+      handler(newvalue){
+        console.log(newvalue)
+      },
+      // 객체일때 바뀌는걸 확인할수있게해줌
+      deep : true
+    },
+    name : {
+      handler(newvalue){
+        console.log(newvalue);
+      },
+      // 바뀌어야만 체크를해주는데 이거하면 첫값도 체크해줌
+      immediate : true
     }
+  },
+  computed:{
+  },
+  directives:{
+    
+  },
+  methods:{
   }
 }
 </script>
 
 <style>
+
 </style>
